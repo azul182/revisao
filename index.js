af6e9372b1ec8cb3d63c38dc25c280dcb8bd6ce7
azul182 <84513231+azul182@users.noreply.github.com>
2022-08-01 12:43:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//caregar o módulo express 
+const express = require('express')
+
+//carregar o módulo mongoose
+const mongoose = require('mongoose')
+
+//conectar com o banco de dados revisao 
+const conexao = ()=>{
+    mongoose.connect('mongodb+srv://userRevisao:thi020206@cluster0.s3hds.mongodb.net/revisao')
+}
+//conectar com a collection infos 
+const modelo = new mongoose.Schema({
+    nome:String,
+    turma:String,
+    disciplina:String
+})
+const infos = mongoose.model('infos', modelo)
+
+//executar o módulo do express  
+const app = express()
+
+//definir o local padrão para os arquivos ejs
+app.set('views','./')
+
+//renderizar o arquivo index.ejs na requisição / (root)
+app.get('/',async(req,res)=>{
+    //conectar com o revisao 
+    conexao()
+    //buscar todos os dados de infos
+    const resultado = await infos.find()
+    res.render('index.ejs',{resultado})
+})
+
+//ligar o servidor na porta 3050
+app.listen(3050, ()=>{
+    console.log('servidor local em http://localhost:3050')
+})
